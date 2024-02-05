98733eb528bb8ec3f63e806b6210746315408f68
balanced <vladyslav.herasymenko@infotech.gov.ua>
2024-02-05 18:06:59 +0200

    ci: update versions of actions in build.yml

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-#![deny(missing_docs)]
-#![deny(clippy::all)]
-#![deny(clippy::pedantic)]
-#![deny(clippy::nursery)]
-#![deny(clippy::cargo)]
-#![forbid(unsafe_code)]
-#![allow(clippy::needless_pass_by_value)]
-#![allow(clippy::module_name_repetitions)]
-#![allow(clippy::items_after_statements)]
-#![allow(clippy::multiple_crate_versions)]
-#![allow(clippy::unused_async)]
-#![doc = include_str!("../README.md")]
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
 
 fn main() {
     println!("Hello, world!");
