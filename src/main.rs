bfc3b693aa3b3260275868384c51486321abd551
balanced <vladyslav.herasymenko@infotech.gov.ua>
2024-02-18 17:26:32 +0200

    feat(calculator): create struct for expression and trait for methods

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 
+pub struct ExpressionEvaluator {
+    pub expression: String,
+}
+
+pub trait Evaluatable {
+    fn new(expression: String) -> Self;
+    fn evaluate(&self) -> i32;
+}
+
+impl Evaluatable for ExpressionEvaluator {
+    fn new(expression: String) -> Self {
+        Self { expression }
+    }
+
+    fn evaluate(&self) -> i32 {
+        0
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let expression = String::from("2 * 3 + 4");
+    let expression_evaluator = ExpressionEvaluator::new(expression);
+    println!("{}", expression_evaluator.evaluate());
 }
