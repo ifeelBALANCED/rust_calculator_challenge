d7658d616bfb0ea0d0a16416f505bed2a60187ed
balanced <vladyslav.herasymenko@infotech.gov.ua>
2024-02-18 18:02:11 +0200

    fix: make import from main crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
-mod expression_evaluator;
-use expression_evaluator::{Evaluatable, ExpressionEvaluator};
+
+use rust_calculator_challenge::expression_evaluator::{Evaluatable, ExpressionEvaluator};
 
 fn main() {
     let expression = String::from("2 * 3 + 4");
